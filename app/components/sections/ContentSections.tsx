ee042ad9c032d2a8f682a10ec1ecded0971c4e05
Tom Zheng <tomzheng1012@gmail.com>
2025-06-22 01:35:02 -0700

    update swimming

--- app/components/sections/ContentSections.tsx
+++ app/components/sections/ContentSections.tsx
@@ -32,7 +32,7 @@ export default function ContentSections({ baseDelay, delayIncrement }: ContentSe
 
       <section aria-labelledby="thinking thoughts" className="mt-10 mb-8">
       <FadeIn delay={baseDelay + delayIncrement * 4}>
-        <h2 id="thinking " className="text-xl mb-2">i also like to swim, gym, read, and create experiences.</h2>
+        <h2 id="thinking " className="text-xl mb-2">i also swim (competitively), gym, read, and love creating experiences.</h2>
       </FadeIn>
       </section>
     </>
